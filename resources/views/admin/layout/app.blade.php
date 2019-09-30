260f0829477f163562330862942cfac6ff627a27
Rahul Kewate <rahul.kewate@gmail.com>
2019-09-30 20:21:58 +0530

    first commit

---
+++ resources/views/admin/layout/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Header -->
+@include('admin.inc.header')
+
+<!-- sidemenu -->
+@include('admin.inc.sidemenu')
+
+        <!-- content -->
+@yield('content')
+
+<!-- footer -->
+@include('admin.inc.footer')      
+</html>
